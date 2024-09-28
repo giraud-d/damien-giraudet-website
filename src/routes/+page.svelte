798152670da6e1ebc2061d4f8b031b94cbf942e8
damien <damien@giraudet.me>
2024-09-28 11:03:30 +0200

    ci: test auto deploy 3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Damien giraudet</h1>
 <h2>Formateur</h2>
 <h4>Ami de Julien et Pierre-antoine</h4>
-<p>Deploy via github push on main v2</p>
+<p>Deploy via github push on main v3</p>
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
