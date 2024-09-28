0e6b13bc2ddfbf9f24ffeca1e0cc719f7fd64413
damien <damien@giraudet.me>
2024-09-28 10:22:13 +0200

    feat: change html data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Damien giraudet</h1>
+<h2>Formateur</h2>
+<h4>Ami de Julien et Pierre-antoine</h4>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
